543a5925dde70773a175467497a395358f3a228f
luyu <398242754@qq.com>
2022-11-19 10:20:33 +0800

    master test

--- src/main/java/com/tyut/test/HelloGit.java
+++ src/main/java/com/tyut/test/HelloGit.java
@@ -3,6 +3,6 @@ package com.tyut.test;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("master!!");
-        System.out.println("hello got");
+        System.out.println("hello hot");
     }
 }
